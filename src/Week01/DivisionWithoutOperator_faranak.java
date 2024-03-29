0e097a4cad59ce8ffdcebf19330a51e2a5f99567
faranak <faranak.jesus@yahoo.com>
2024-03-28 21:25:19 -0700

    updated

--- src/Week01/DivisionWithoutOperator_faranak.java
+++ src/Week01/DivisionWithoutOperator_faranak.java
@@ -4,7 +4,7 @@ public class DivisionWithoutOperator_faranak {
 
     public static void main(String[] args) {
 
-        division(10, 0);
+        division(10, 2);
     }
     public static void division(int a, int b) {
 
