3b5d708da4350a74eafc7f6b693976063c4b2952
faranak <faranak.jesus@yahoo.com>
2024-04-12 12:12:52 -0700

    week 4 tasks

---
+++ src/week04/FrequencyOfCharacters.java
@@ -0,0 +1,21 @@
+package week04;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public record FrequencyOfCharacters() {
+
+    public static void main(String[] args) {
+        String input = "AAABBCDD";
+        Map<Character,Integer> frequencies = new HashMap<>();
+        for (char ch : input.toCharArray())
+            frequencies.merge(ch, 1, Integer::sum);
+        System.out.println(frequencies);
+        }
+             }
+/*
+Write a return method that can find the frequency of
+characters
+Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
